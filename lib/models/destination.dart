728f9494be0e7b530f82c9c5c350e7d3574909c8
tuyisengeaurele <157024701+tuyisengeaurele@users.noreply.github.com>
2026-02-26 19:19:17 +0200

    updated the app's model

---
+++ lib/models/destination.dart
@@ -0,0 +1,23 @@
+class Destination {
+  final String id;
+  final String name;
+  final String location;
+  final String description;
+  final double price;
+  final double rating;
+  final String image;
+  final String category;
+  final int days;
+
+  const Destination({
+    required this.id,
+    required this.name,
+    required this.location,
+    required this.description,
+    required this.price,
+    required this.rating,
+    required this.image,
+    required this.category,
+    required this.days,
+  });
+}
